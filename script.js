ccc9fc65cdcc393971a7819e79780766c4ad972e
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-07 11:48:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+console.log("Hallo Winc Academy Studenten")
+
+//dit is een grote som
+let keer = 1230941 * 1823794;
+console.log(keer);
+let delen = 637263 / 54;
+console.log(delen);
+
+// antwoord vraag 4: je ziet de code wel omdat je alleen een opmerking boven de som hebt gezet en niet de som zelf een comment hebt mee gegeven.
+
+/*
+let nogeenkeer = 1230941 * 1823794;
+console.log(nogeenkeer);
+let nogmaalsdelen = 637263 / 54;
+console.log(nogmaalsdelen);*/
+
+//antwoord op vraag 6: je ziet de code nu niet omdat je van de gehele code een comment hebt gemaakt.
