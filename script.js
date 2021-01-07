c421b61b12c2437bac9cb6f3b3b6679842511b89
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-07 11:50:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,13 @@
 console.log("Hallo Winc Academy Studenten")
 
 //dit is een grote som
-let keer = 1230941 * 1823794;
-console.log(keer);
-let delen = 637263 / 54;
-console.log(delen);
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
 
 // antwoord vraag 4: je ziet de code wel omdat je alleen een opmerking boven de som hebt gezet en niet de som zelf een comment hebt mee gegeven.
 
 /*
-let nogeenkeer = 1230941 * 1823794;
-console.log(nogeenkeer);
-let nogmaalsdelen = 637263 / 54;
-console.log(nogmaalsdelen);*/
+console.log(1230941 * 1823794);
+console.log(637263 / 54);*/
 
 //antwoord op vraag 6: je ziet de code nu niet omdat je van de gehele code een comment hebt gemaakt.
